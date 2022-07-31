0ebc69348f8a35761bc7d345f96eb47a8832a447
Quinten Bruce <qcbruce7@gmail.com>
2022-07-31 09:03:36 -0500

    first commit

---
+++ Capstone.v
@@ -0,0 +1,25 @@
+module Top (input St, input CLK); // control circuit
+	
+endmodule
+
+
+module Accumulator (input Si, input Sh, output Xi);
+
+endmodule
+
+module Addend (input Si, input SH, output Yi); //
+
+endmodule
+
+module FullAdder (input x, input y, input Ci, output Si, output Cout); //full adder with flip flop
+
+endmodule
+
+
+
+
+
+
+module testbench;
+	
+endmodule
