f3b9b3b1634970ab4a42cd5ecedd858eda932307
Quinten Bruce <qcbruce7@gmail.com>
2022-08-04 14:43:28 -0500

    yes

--- Capstone.v
+++ Capstone.v
@@ -1,47 +1,37 @@
 //accumulator right shift register
 module accumulator_shift_reg (input CLK, //Clock
 							  input Si, //Serial in
-							  //input Sh, //shift right 
 							  input L, //Load enable
 							  input [7:0] Load, //Load value
-							  output Sout //Shift out
+							  input PoutE, //Parralell out enable
+							  output Sout, //Shift out
+							  output reg [7:0] Pout //Parralell out
 							  ); 
-
 	reg Sout;
 	reg [7:0] accumulator;
 
 	always @ (CLK) begin
 		if (L) begin 
 			accumulator = Load;
-		$display("Accumulator Loaded: %b", accumulator);
 		end
-			
+		
 	end
 
 	always @ (posedge CLK) begin
-		if (!L) begin
-			$display("\nPositive Edge");
-		
-		Sout = accumulator[0];
-		$display("Accumulator: %b Sout: %b", accumulator, Sout);
-		end
-		
+		if (!L)
+			Sout = accumulator[0];
+		if (PoutE)
+			Pout = accumulator;
 	end
 
 	always @ (negedge CLK) begin
-		if (!L) begin
-			$display("\nNegative Edge");
-		//if (Sh) begin
+		if (!L)
 			accumulator = {Si, accumulator[7:1]};
-		//end
-		$display("Accumulator: %b Si: %b", accumulator, Si);
-		end
-		
+
+		if (PoutE)
+			Pout = accumulator;
 	end
 endmodule
-
-
-
 
 // cyclical shift register
 module addend_shift_reg (input CLK, //Clock
@@ -53,34 +43,19 @@ module addend_shift_reg (input CLK, //Clock
 	reg Sout;
 	reg [7:0] subtrahend;
 
-
-
 	always @ (CLK) begin
 		if (L) begin
 			subtrahend = Load;
-
-			$display("Subtrahend Loaded: %b", subtrahend);
 		end
-			
 	end	
 	
 	always @ (posedge CLK) begin
-		if (!L) begin
-			$display("\nPositive Edge");
-		
-		Sout = subtrahend[0];
-		$display("Subtrahend: %b Sout: %b", subtrahend, Sout);
-		end
-		
+		if (!L)
+			Sout = subtrahend[0];
 	end	
 	always @ (negedge CLK) begin
-		if (!L) begin
-			$display("\nNegative Edge");
-		
-		subtrahend = {subtrahend[0], subtrahend[7:1]};
-		$display("Subtrahend: %b", subtrahend);
-		end
-		
+		if (!L) 
+			subtrahend = {subtrahend[0], subtrahend[7:1]};
 	end
 endmodule
 
@@ -88,24 +63,16 @@ endmodule
 //Module for borrow flipflop
 module borrow_flipflop (input D, //Input
                        input CLK, 
-					   input R,
+					   input R, //Reset
                        output reg Q //Output
-			
 					   ); 
-	
-
     always @(negedge CLK) begin //executes block at every neg edge of clock // print current values to output console
         if (!R)
 			Q = D;
 		else
 			Q = 0;
     end
-
 endmodule
-
-
-
-
 
 //Module for full subtractor
 module full_subtractor (input X, //Minuend
@@ -117,37 +84,29 @@ module full_subtractor (input X, //Minuend
     
     assign D = X ^ Y ^ Bin; // D(x, y, Bin) = x ⊕ y ⊕ Bin
     assign Bout = (~X & Bin) | (Y & Bin) | (~X & Y); //Bout(x, y, Bin) = x'*Bin + Y*Bin + x'y
-	always @ (Bout) $display("X: %b Y: %b Bin: %b D: %b Bout: %b", X, Y, Bin, D, Bout);
-
-	
-
 endmodule
 
-
-
-
-
 module top (input St, //start signal
-			output Sh //Control circuit
+			input [7:0] eightBitMinuend,
+			input [7:0] eightBitSubtrahend,
+			output [7:0] eightBitDifference
 			);
-	
+	reg [7:0] acc;
 	reg CLK = 0;
 	reg LAccumulator = 1;
 	wire minuend;
-	reg [7:0] LoadAcc = 8'b00101101;
+	reg PoutE = 0;
+	wire [7:0] Difference;
 
 	accumulator_shift_reg accumulatorReg(
-		.CLK(CLK), .Si(Si), .L(LAccumulator), .Load(LoadAcc), .Sout(minuend)
+		.CLK(CLK), .Si(Si), .L(LAccumulator), .Load(eightBitMinuend), .Sout(minuend), .PoutE(PoutE), .Pout(eightBitDifference)
 	);
-
-
 
 	reg LSubtrahend = 1;
 	wire subtrahend;
-	reg [7:0] LoadSubtrahend = 8'b11010000;
 
 	addend_shift_reg subtrahendReg(
-		.CLK(CLK), .L(LSubtrahend), .Load(LoadSubtrahend), .Sout(subtrahend)
+		.CLK(CLK), .L(LSubtrahend), .Load(eightBitSubtrahend), .Sout(subtrahend)
 	);
 
 	wire Bout;
@@ -158,41 +117,35 @@ module top (input St, //start signal
 		.D(Bout), .CLK(CLK), .R(R), .Q(Bin)
 	);
 
-
 	full_subtractor subtractor(
 		.X(minuend), .Y(subtrahend), .Bin(Bin), .D(Si), .Bout(Bout)
 	);
 
-	
-
+	initial begin
+		$dumpfile("sss.vcd");
+		$dumpvars(0, top);
+	end
 	
 	always begin
-		$display("\nCLK: %b Min: %b, Sub: %b, D: %b Bout: %b",CLK,  minuend, subtrahend, Si, Bout);
-		$monitor("Si: %b Bout: %b", Si, Bout);
 		#10 
 		R = 0;
 		CLK = 0;
 		LAccumulator = 0;
 		LSubtrahend = 0;
+		//$display("\nCLK: %b Min: %b, Sub: %b, D: %b Bout: %b",CLK,  minuend, subtrahend, Si, Bout);
 		#10
-		R = 0;
 		CLK = ~CLK;
-
-		
 	end
 
 
-	always #180 $finish;
-	
+	always #180 begin
+		PoutE = 1;
+		#10
+		//$display("Accu: ", Difference);
+		$finish;
 
-	
-
-
+	end
 endmodule
-
-
-
-
 
 /////////////////////////////
 ////top module Testbench////
@@ -200,5 +153,25 @@ endmodule
 
 
 module topTB; //Testbench for control circuit
-    
+    reg St; //Input registers
+
+	reg [7:0] eightBitMinuend = 8'b10100101 ;
+	reg [7:0] eightBitSubtrahend = 8'b10000011;
+	wire [7:0] eightBitDifference;
+
+	top UUT(.St(St), .eightBitMinuend(eightBitMinuend), .eightBitSubtrahend(eightBitSubtrahend), .eightBitDifference(eightBitDifference)); // Unit Under Test
+	// Test Execution
+	initial begin
+		#190
+		$display(" %b {%d}", eightBitMinuend, eightBitMinuend);
+		$display("-%b {%d}", eightBitSubtrahend, eightBitSubtrahend);
+		$display("------------");
+		$display(" %b {%d}\n\n", eightBitDifference, eightBitDifference);
+		// #190
+		// 
+		// $display(" %b {%d}", eightBitMinuend, eightBitMinuend);
+		// $display("-%b {%d}", eightBitSubtrahend, eightBitSubtrahend);
+		// $display("------------");
+		// $display(" %b {%d}\n\n", eightBitDifference, );
+	end
 endmodule
