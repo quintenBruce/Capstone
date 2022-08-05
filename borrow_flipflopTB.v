fd4b41e932bd5b166f387625a1338f7606b53c8a
Quinten Bruce <qcbruce7@gmail.com>
2022-08-04 22:19:10 -0500

    fix flip flop TB

--- borrow_flipflopTB.v
+++ borrow_flipflopTB.v
@@ -4,19 +4,17 @@
 module borrow_flipflopTB; //Testbench for borrow flip flop module
     reg D, CLK; 
 	wire Q;
-	borrow_flipflop UUT(D, CLK, Q);
+	reg R = 0;
+	borrow_flipflop UUT(D, CLK, R, Q);
 	always begin
         #10;
-        
-        
-        $monitor("%b  %b  %b| %b", D, CLK, Q); 
+        $monitor("%b   %b| %b", D, CLK, Q); 
 		{D, CLK} = {D, CLK} + 1;
-		
     end
         
 	initial begin
 	    {D, CLK} = 2'b00; 
-		$display("D CLK  R| Q"); 	
+		$display("D CLK | Q"); 	
 	end
     initial
 		#90 $finish; 
